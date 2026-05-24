cdc53c096d83811bc534fc98185ff1b9950d6663
paulohenriquecosta3011 <paulohenriquecosta@hotmail.com>
2026-05-24 16:18:40 -0300

    Add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+//vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-   server: {
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "PHCore",
+        short_name: "PHCore",
+        description:
+          "Sistema de gestão de convidados e convites",
+
+        theme_color: "#111827",
+        background_color: "#ffffff",
+
+        display: "standalone",
+
+        start_url: "/",
+
+        icons: [
+          {
+            src: "/pwa-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+
+          {
+            src: "/pwa-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+
+  server: {
     host: true,
-    port: 5173
-  }
-})
+    port: 5173,
+  },
+});
